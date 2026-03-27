34df54901be00e8795854e69dc25e375c805d50f
Gabriela Benevides <147735318+gabrielamqs@users.noreply.github.com>
2026-03-27 09:05:53 -0300

    Fix formatting of base URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/ecto-ecommerce/'
+  base: '/ecto-ecommerce/',
   server: {
     host: "::",
     port: 8080,
